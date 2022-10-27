ec5422c3dd488303bc25d9123057fa9532a97114
Gabriella Fujita <gabriela.fujita@hotmail.com>
2022-10-27 18:01:41 -0400

    Integraçao com testes, pronto para debug de calendar

---
+++ src/br/com/fiap/teste/PessoaDaoTeste.java
@@ -0,0 +1,34 @@
+package br.com.fiap.teste;
+
+
+import br.com.fiap.bean.Pessoa;
+import br.com.fiap.dao.OraclePessoaDAO;
+
+import java.util.Calendar;
+
+public class PessoaDaoTeste {
+    public static void main(String[] args){
+        //Instancia o DAO
+        OraclePessoaDAO dao = new OraclePessoaDAO();
+
+        //Instancia a Pessoa
+       Pessoa pessoa= new Pessoa(1, "teste", "teste", Calendar.getInstance(),"email@teste");
+
+
+        //Cadastra no banco de dados
+        dao.cadastrar(pessoa);
+
+        //Lista as Pessoas
+        dao.listar();
+        System.out.println(dao.listar());
+
+        //atualizar a Pessoa
+        dao.atualizar(pessoa);
+
+        //Buscar por Id
+        dao.buscarPorId(pessoa.getCodigo());
+
+        System.out.println("Pessoa testado!");
+
+    }
+}
